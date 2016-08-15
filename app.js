7a42816cc2135250e66b1cc9b0021a34b2210d3f
jeanayee <jeana.yee@gmail.com>
2016-08-14 21:30:10 -0400

    v2

--- app.js
+++ app.js
@@ -1,13 +1,4 @@
 var main = function() {
-  //Sticky navigation
-  var $nav = $('.navigation')
-  var posTop = $nav.position().top;
-  $(window).scroll(function () {
-    var y = $(this).scrollTop();
-    if (y > posTop) {
-      $nav.addClass('fixed');
-    } else { $nav.removeClass('fixed'); }
-  });
 
   //Next Arrow
   $('.arrow-next').click(function() {
